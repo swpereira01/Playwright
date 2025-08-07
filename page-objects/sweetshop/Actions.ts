0a61c1e68d37990ce9e9c3023e055e7115a8175b
swpereira01 <swp2015@outlook.com>
2025-08-06 22:28:32 -0400

    P-6RemoveItemfromBasket

--- page-objects/sweetshop/Actions.ts
+++ page-objects/sweetshop/Actions.ts
@@ -4,13 +4,19 @@ export class Actions {
     readonly page;
     readonly browseSweetsButton;
     readonly navigateToBasketButton;
+    readonly numberOfItems;
     readonly basket;
     readonly itemPick;
+    readonly basketItem;
+    readonly deleteButton;
     constructor(page: Page) {
         this.page = page;
         this.navigateToBasketButton = this.page.getByRole('link', { name: 'Basket' });
-        this.basket = this.page.locator('[id="basketItems"]');;
+        this.numberOfItems = this.page.locator('#basketCount');
+        this.basket = this.page.locator('#basketItems');
         this.itemPick = (itemName: string) => this.page.locator('div.card').locator('div.card-footer').locator(`[data-name="${itemName}"]`);
+        this.basketItem = (itemName: string) => this.basket.locator('li').getByText(`${itemName}`);
+        this.deleteButton = (itemName: string) => this.page.getByRole('listitem').filter({ hasText: `${itemName}` }).getByRole('link');
 
 
     }
@@ -20,11 +26,27 @@ export class Actions {
             await item.click();
         }
     }
+    async numberOfItemsInBasket(number: string) {
+        await expect(this.numberOfItems).toHaveText(number);
+    }
     async navigateToBasket() {
         await expect(this.navigateToBasketButton).toBeAttached();
         await this.navigateToBasketButton.click()
     }
     async verifyItemsInBasket(itemName) {
         await expect(this.basket).toContainText(itemName);
+    }
+    async removeItemFromBasket(itemName: string) {
+        let itemToDelete = this.basketItem(itemName);
+        await expect(itemToDelete).toBeVisible();
+        let itemToDeleteButton = this.deleteButton(itemName)
+        await itemToDeleteButton.dblclick(); //workaround - the tests are very flaky with one click
+    }
+    async acceptRemoval() {
+        this.page.on('dialog', async dialog => {
+            await dialog.message();
+            console.log(dialog.message());
+            await dialog.accept();
+        })
     }
 }
--- page-objects/sweetshop/Homepage.ts
+++ page-objects/sweetshop/Homepage.ts
@@ -1,13 +1,13 @@
-import {expect, type Locator, type Page } from '@playwright/test';
+import { expect, type Locator, Page } from '@playwright/test';
 
-export class Homepage{
-    readonly page:Page;
-    goToUrl: string = 'https://sweetshop.vivrichards.co.uk/';
-  
- constructor(page: Page) {
-    this.page = page;
- }
- async goto() {
-    await this.page.goto(this.goToUrl);
- }
+export class Homepage {
+   readonly page: Page;
+   goToUrl: string = 'https://sweetshop.vivrichards.co.uk/';
+
+   constructor(page: Page) {
+      this.page = page;
+   }
+   async goto() {
+      await this.page.goto(this.goToUrl);
+   }
 }
